ba74a2b0ea2b2113adf3b813c25d62dc9a98b757
sripriya497 <sripriyadharshini497@gmail.com>
2025-04-08 11:29:07 -0700

    Updated S3+Cloudfront module

--- modules/s3-static-website/main.tf
+++ modules/s3-static-website/main.tf
@@ -1,1 +1,28 @@
- 
+ resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*"
+    }
+  ]
+}
+EOT
+}
